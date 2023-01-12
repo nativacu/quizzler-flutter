c1c0dbc1120f52f041f32a3bbbfe3009cf11a4b2
nativacu <nativacu@gmail.com>
2023-01-11 21:56:09 -0400

    feat: displaying questions and updating answer icons

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,31 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  Icon correctAnswerIcon = Icon(Icons.check, color: Colors.green);
+  Icon wrongAnswerIcon = Icon(Icons.close, color: Colors.red);
+
+  List<Icon> answers = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
+  List<bool> correctAnswer = [false, true, true];
+
+  int questionIndex = 0;
+
+  Icon getAnswerIcon(bool response) {
+    return response == correctAnswer[questionIndex]
+        ? correctAnswerIcon
+        : wrongAnswerIcon;
+  }
+
+  void updateAnswers(bool response) {
+    answers.add(getAnswerIcon(response));
+    questionIndex = (questionIndex++) % questions.length;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +62,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionIndex],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +87,9 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                setState(() {
+                  updateAnswers(true);
+                });
                 //The user picked true.
               },
             ),
@@ -82,12 +110,17 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                setState(() {
+                  updateAnswers(false);
+                });
                 //The user picked false.
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: answers,
+        )
       ],
     );
   }
